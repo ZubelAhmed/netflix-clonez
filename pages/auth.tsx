9690553242edcb9298a2734758efba88a5f79dca
ZubelAhmed <zubel.cse@gmail.com>
2024-11-08 23:13:27 +0100

    Google Git hub login - logout

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -106,6 +106,7 @@ console.log(error);
 
 <div className="flex flex-row items-center gap-4 mt-8 justify-center">
 <div 
+onClick={() => signIn('google',{callbackUrl:'/'})}
 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition" >
 
 <FcGoogle size={30} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 
+import { NextPageContext } from "next";
+import { getServerSession } from "next-auth";
+import { getSession, signOut } from "next-auth/react"
+
+export async function getServerSideProps(context:NextPageContext) {
+const session =await getSession(context);
+
+if(!session) {
+
+return {
+redirect:{
+destination: '/auth',
+parmanent:false,
+}
+}
+}
+return {
+props: {}
+}
+}
 
 export default function Home() {
   return (
         <>
-        <h1 className="text-2xl text-green-500">Netflix Clone... </h1>
+        <h1 className="text-4xl text-green-500">Netflix Clone... </h1>
+        <button className="h-10 w-full bg-white" onClick={()=> signOut()}>Logout!</button>
         </>
   );
 }
