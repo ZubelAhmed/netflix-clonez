8f39aefab22dd366d6f574b7409bac02a0b3edca
ZubelAhmed <zubel.cse@gmail.com>
2024-11-10 00:09:02 +0100

    redirect-to profiles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 
+import { countReset } from "console";
+import useCurrentUser from "hooks/userCurrentUser";
 import { NextPageContext } from "next";
 import { getServerSession } from "next-auth";
 import { getSession, signOut } from "next-auth/react"
 
 export async function getServerSideProps(context:NextPageContext) {
-const session =await getSession(context);
+const session =await getSession(context); 
 
 if(!session) {
 
@@ -21,9 +23,12 @@ props: {}
 }
 
 export default function Home() {
+
+  const { data:user } = useCurrentUser();
   return (
         <>
         <h1 className="text-4xl text-green-500">Netflix Clone... </h1>
+        <p className="text-white">Logged in as : {user?.name}</p>
         <button className="h-10 w-full bg-white" onClick={()=> signOut()}>Logout!</button>
         </>
   );
