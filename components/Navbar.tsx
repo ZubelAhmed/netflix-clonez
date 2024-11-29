ffef22bf26db3a840c69e6efbbe64daf7ba7dea2
ZubelAhmed <zubel.cse@gmail.com>
2024-11-29 23:01:25 +0100

    Billboard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {BsChevronDown,BsSearch,BsBell} from 'react-icons/bs'
 
 
@@ -7,10 +7,31 @@ import NavbarItem from "./NavbarItem"
 import MobileMenu from "./MobileMenu";
 import AccountMenu from "./AccountMenu";
 
+const TOP_OFFSET = 66;
+
 const Navbar = () => {
 
  const [showMobileMenu, setShowMobileMenu] =useState(false);
  const [showAccountMenu, setshowAccountMenu] =useState(false);
+const [showBackground, setShowBackground] =useState(false);
+
+ useEffect(()=> {
+const handleScroll=() => {
+
+if(window.scrollY>= TOP_OFFSET) {
+    setShowBackground(true);
+} else {
+    setShowBackground(false);
+}
+
+}
+
+window.addEventListener('scroll',handleScroll);
+return() => {
+window.removeEventListener('scroll', handleScroll)
+}
+
+},[]);
 
 
  const toggleMobileMenu = useCallback(() => {
@@ -27,7 +48,7 @@ setshowAccountMenu((current) => !current);
 return (
     
     <nav className="w-full fixed z-40">
-       <div className="
+       <div className={`
        px-4
        md:px-16
        py-6
@@ -36,9 +57,9 @@ return (
        items-center
        transition
        duration-500
-       bg-zinc-900
-       bg-opacity-90
-       " >
+       ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}
+     `}
+        >
 
         <img className="h-4 lg:h-7" src ="/images/logo.png" alt="Logo"/>
         <div
@@ -60,7 +81,7 @@ return (
              </div>
              <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
 <p className="text-white text-sm">Browse</p>
-<BsChevronDown className="text-white transition" />
+<BsChevronDown className={`text-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} />
 <MobileMenu visible={showMobileMenu} />
              </div>
              <div className="flex flex-row ml-auto gap-7 items-center">
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { NextPageContext } from "next";
 import { getServerSession } from "next-auth";
 import { getSession } from "next-auth/react"
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 export async function getServerSideProps(context:NextPageContext) {
 const session =await getSession(context); 
 
@@ -29,6 +30,7 @@ export default function Home() {
   return (
         <>
         <Navbar/>
+        <Billboard />
         </>
   );
 }
